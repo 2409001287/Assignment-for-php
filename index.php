9ee9033d3ec5ec5a6fd778aa7a1ce9af7efd0e2a
BYIRINGIRO Placide <byiringirop@asome.health>
2025-04-18 18:26:12 +0200

    This assignment is about Getting Started with php

---
+++ index.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>BYIRINGIRO Placide PHP</title>
+</head>
+<body>
+    <h1>BYIRINGIRO Placide PHP</h1>
+
+    <pre>
+    B B B
+    B     B
+    B B B
+    B     B
+    B B B
+    </pre>
+
+    <p>The SHA256 hash of "BYIRINGIRO Placide" is:</p>
+    <p>
+        <?php
+            print hash('sha256', 'BYIRINGIRO Placide');
+        ?>
+    </p>
+
+    <p><a href="check.php">Click here to run check.php</a></p>
+    <p><a href="fail.php">Click here to run fail.php</a></p>
+</body>
+</html>
